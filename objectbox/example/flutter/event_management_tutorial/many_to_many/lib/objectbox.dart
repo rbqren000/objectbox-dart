019f62e5707feb54e7e410ef42db4aae01db9565
Uwe <noreply+uwe@objectbox.io>
2026-06-09 12:13:07 +0200

    Event manager examples: app specific dir on desktop, macOS app group

--- objectbox/example/flutter/event_management_tutorial/many_to_many/lib/objectbox.dart
+++ objectbox/example/flutter/event_management_tutorial/many_to_many/lib/objectbox.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
 import 'model.dart';
 import 'objectbox.g.dart';
 
@@ -31,8 +34,21 @@ class ObjectBox {
 
   /// Create an instance of ObjectBox to use throughout the app.
   static Future<ObjectBox> create() async {
+    // Note: setting a unique directory is recommended if running on desktop
+    // platforms. If none is specified, the default directory is created in the
+    // users documents directory, which will not be unique between apps.
+    // On mobile this is typically fine, as each app has its own directory
+    // structure.
+
+    // Note: set macosApplicationGroup for sandboxed macOS applications, see the
+    // info boxes at https://docs.objectbox.io/getting-started for details.
+
     // Future<Store> openStore() {...} is defined in the generated objectbox.g.dart
-    final store = await openStore();
+    final store = await openStore(
+        directory: p.join((await getApplicationDocumentsDirectory()).path,
+            "event_manager_objectbox"),
+        macosApplicationGroup: "objectbox.demo");
+
     return ObjectBox._create(store);
   }
 
